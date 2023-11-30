6b5a2391c425556da3a0013ce36ed4ecde43a64c
Chris Oleson <crolesonjr@gmail.com>
2023-11-30 15:17:39 -0700

    adding project details

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
         <a href="https://github.com/chris-oleson" target="_blank"><img height="32" width="32" src="https://cdn.simpleicons.org/github/202d3b"/></a>
         <a href="https://www.linkedin.com/in/chris-oleson-8372b2160/" target="_blank"><img height="32" width="32" src="https://cdn.simpleicons.org/linkedin/202d3b"/></a>
     </div>
-    
+
     <div class="container">
         <div class="title">
             <h1>Chris Oleson</h1>
@@ -14,14 +14,37 @@
                 <p>Entrepreneur</p>
             </div>
         </div>
-        
+
         <div class="projects">
             <h2>Projects</h2>
             <hr>
-            <a href="https://vufi.app" target="_blank">VuFi</a>
+            <a @click="display('vufi')">VuFi</a>
+        </div>
+
+        <div class="details">
+            <Transition mode="out-in">
+                <div v-if="details == 'vufi'">
+                    <p>VuFi is a full stack commercial web application that automatically tracks and visualizes financial asset data. The frontend is built with Vue, and the backend is built with Express, Node, and MySQL</p>
+                    <a href="https://vufi.app" target="_blank">View ➤</a>
+                </div>
+            </Transition>
         </div>
     </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+const details = ref('')
+
+function display(project) {
+    if (project == details.value) {
+        details.value = ''
+    }
+    else {
+        details.value = project
+    }
+}
+</script>
 
 <style>
 body {
@@ -35,38 +58,39 @@ a {
     text-decoration: inherit;
     transition-duration: 200ms;
 }
-a:hover {
-    filter: brightness(150%);
-    transform: scale(1.05);
-}
 
 #header {
     display: flex;
     justify-content: right;
     align-content: center;
-    margin-bottom: 100px;
 
     a {
         margin: auto 10px auto 10px;
     }
+    a:hover {
+        filter: brightness(150%);
+        transform: scale(1.05);
+    }
 }
 
 .container {
+    height: 80vh;
     display: flex;
-    justify-content: center;
+    justify-content: space-evenly;
     align-items: center;
     flex-wrap: wrap;
-    margin-bottom: 100px;
+    margin-bottom: 50px;
 }
 
 .title {
-    margin: 100px;
+    margin: 0px 50px 20px 50px;
     text-align: center;
-
+    
     h1 {
         font-size: 96px;
         margin: 0;
         font-weight: 100;
+        text-shadow: 7px 7px 6px rgba(136, 136, 136, 0.5);
     }
 
     .subtitle {
@@ -81,7 +105,6 @@ a:hover {
 }
 
 .projects {
-    margin: 100px;
     text-align: center;
 
     h2 {
@@ -96,9 +119,35 @@ a:hover {
     }
 
     a {
+        user-select: none;
+        cursor: pointer;
         font-size: 20px;
         font-weight: 400;
         display: block;
     }
+    a:hover {
+        transform: scale(1.05);
+        text-shadow: 4px 4px 3px rgba(136, 136, 136, 0.5);
+    }
+}
+
+.details {
+    width: 300px;
+    transition-duration: 200ms;
+    a {
+        display:block;
+        font-weight: 400;
+        text-align: right;
+    }
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.2s;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
