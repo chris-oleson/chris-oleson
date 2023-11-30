00bf3a1e9e4d219fd0bb289199745550dc3f22d3
Chris Oleson <crolesonjr@gmail.com>
2023-11-30 01:20:48 -0700

    removing vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,104 @@
 <template>
-    <v-app>
-        <v-app-bar class="bg-background" flat>
-            <v-spacer></v-spacer>
-            <v-btn class="font-weight-light text-none" variant="plain" href="mailto:crolesonjr@gmail.com">crolesonjr@gmail.com</v-btn>
-            <v-btn icon variant="plain" href="https://github.com/chris-oleson" target="_blank"><img height="32" width="32" src="https://cdn.simpleicons.org/github/ffffff"/></v-btn>
-            <v-btn class="mx-2" icon variant="plain" href="https://www.linkedin.com/in/chris-oleson-8372b2160/" target="_blank"><img height="32" width="32" src="https://cdn.simpleicons.org/linkedin/ffffff"/></v-btn>
-        </v-app-bar>
-
-        <v-main>
-            <v-fade-transition appear>
-                <v-container class="fill-height">
-                    <v-row>
-                        <v-col cols="12" sm="6">
-                            <h1 class="text-h1 font-weight-thin text-center mt-n8">Chris Oleson</h1>
-                            <div class="d-flex justify-space-around ma-8">
-                                <p class="font-weight-light">Developer</p>
-                                <p>•</p>
-                                <p class="font-weight-light">Designer</p>
-                                <p>•</p>
-                                <p class="font-weight-light">Entrepreneur</p>
-                            </div>
-                        </v-col>
-
-                        <v-col cols="12" sm="6" class="d-flex justify-center">
-                            <v-list class="text-center">
-                                <v-list-item class="font-weight-thin text-h5">
-                                    Projects
-                                </v-list-item>
-                                <v-divider></v-divider>
-                                <v-list-item>
-                                    <v-btn class="text-none font-weight-light" size="x-large" variant="plain" href="https://vufi.app/" target="_blank">VuFi</v-btn>
-                                </v-list-item>
-                            </v-list>
-                        </v-col>
-                    </v-row>
-                </v-container>
-            </v-fade-transition>
-        </v-main>
-    </v-app>
+    <div id="header">
+        <a href="mailto:crolesonjr@gmail.com">crolesonjr@gmail.com</a>
+        <a href="https://github.com/chris-oleson" target="_blank"><img height="32" width="32" src="https://cdn.simpleicons.org/github/202d3b"/></a>
+        <a href="https://www.linkedin.com/in/chris-oleson-8372b2160/" target="_blank"><img height="32" width="32" src="https://cdn.simpleicons.org/linkedin/202d3b"/></a>
+    </div>
+    
+    <div class="container">
+        <div class="title">
+            <h1>Chris Oleson</h1>
+            <div class="subtitle">
+                <p>Developer</p>
+                <p>Designer</p>
+                <p>Entrepreneur</p>
+            </div>
+        </div>
+        
+        <div class="projects">
+            <h2>Projects</h2>
+            <hr>
+            <a href="https://vufi.app" target="_blank">VuFi</a>
+        </div>
+    </div>
 </template>
 
 <style>
-/* Disable ripple effect */
-.v-ripple__container {
-    opacity: 0 !important;
+body {
+    font-family: Roboto, Geneva, Tahoma, sans-serif;
+    font-weight: 300;
+    color: #202d3b;
 }
 
-/* Removing default link styling */
 a {
     color: inherit;
     text-decoration: inherit;
+    transition-duration: 200ms;
+}
+a:hover {
+    filter: brightness(150%);
+    transform: scale(1.05);
+}
+
+#header {
+    display: flex;
+    justify-content: right;
+    align-content: center;
+    margin-bottom: 100px;
+
+    a {
+        margin: auto 10px auto 10px;
+    }
+}
+
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    margin-bottom: 100px;
+}
+
+.title {
+    margin: 100px;
+    text-align: center;
+
+    h1 {
+        font-size: 96px;
+        margin: 0;
+        font-weight: 100;
+    }
+
+    .subtitle {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
+
+    p {
+        padding: 0px 10px
+    }
+}
+
+.projects {
+    margin: 100px;
+    text-align: center;
+
+    h2 {
+        font-weight: 300;
+        margin: 0px;
+    }
+
+    hr {
+        border: none;
+        height: 1px;
+        background: #202d3b;
+    }
+
+    a {
+        font-size: 20px;
+        font-weight: 400;
+        display: block;
+    }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 const app = createApp(App)
-
-// Vuetify
-import vuetify from './vuetify'
-app.use(vuetify)
-
 app.mount('#app')
