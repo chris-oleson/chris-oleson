b5bcd743c62a5ad1b8e00fd2019f54f738f01fe5
Chris Oleson <crolesonjr@gmail.com>
2024-10-06 00:38:43 -0600

    Refactoring code

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-let details = document.getElementsByClassName('detail')
-function display(project) {
-    let found = false
-    for (let detail of details) {
-        if (detail.classList.contains('show')) {
-            detail.classList.remove('show')
-        }
-        else if (detail.id == project) {
-            detail.classList.add('show')
-            found = true
-        }
+let displayed = 'title'
+function display(project) {   
+    if (project == displayed) {
+        document.getElementsByClassName(project + ' button')[0].classList.remove('clicked')
+        display('title')
     }
-
-    if (found == false) {
-        document.getElementById('title').classList.add('show')
+    else {
+        if (displayed != 'title') {
+            document.getElementsByClassName(displayed + ' button')[0].classList.remove('clicked')
+        }
+        if (project != 'title') {
+            document.getElementsByClassName(project + ' button')[0].classList.add('clicked')
+        }
+        document.getElementsByClassName(displayed + ' detail')[0].classList.remove('show')
+        document.getElementsByClassName(project + ' detail')[0].classList.add('show')
+        displayed = project
     }
 }
 
