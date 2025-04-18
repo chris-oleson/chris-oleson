77f7f3eac0b45625e85f3bda0123914c5e8a272f
Chris Oleson <chris@oleson.family>
2025-04-18 10:09:41 -0600

    changing icon titles

--- components/top-bar.vue
+++ components/top-bar.vue
@@ -8,8 +8,8 @@
             </div>
 
             <div>
-                <FishButton big simple newTab to="https://github.com/chris-oleson" title="github.com/chris-oleson" icon="mdi:github"/>
-                <FishButton big simple newTab to="https://www.linkedin.com/in/chris-oleson" title="https://www.linkedin.com/in/chris-oleson" icon="mdi:linkedin"/>
+                <FishButton big simple newTab to="https://github.com/chris-oleson" title="GitHub" icon="mdi:github"/>
+                <FishButton big simple newTab to="https://www.linkedin.com/in/chris-oleson" title="LinkedIn" icon="mdi:linkedin"/>
                 <FishButton big simple newTab to="https://www.google.com/maps/place/Denver,+CO" title="Denver, Colorado" icon="mdi:location"/>
                 <FishButton big simple to="mailto:chris@oleson.family" title="chris@oleson.family" icon="mdi:email"/>
             </div>
