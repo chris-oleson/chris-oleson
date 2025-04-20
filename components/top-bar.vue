d49aaec84299d47822d80f6f2999f6782bfaff5e
Chris Oleson <chris@oleson.family>
2025-04-19 22:32:14 -0600

    adding stripe link

--- components/top-bar.vue
+++ components/top-bar.vue
@@ -12,6 +12,7 @@
                 <FishButton big simple newTab to="https://www.linkedin.com/in/chris-oleson" title="LinkedIn" icon="mdi:linkedin"/>
                 <FishButton big simple newTab to="https://www.google.com/maps/place/Denver,+CO" title="Denver, Colorado" icon="mdi:location"/>
                 <FishButton big simple to="mailto:chris@oleson.family" title="chris@oleson.family" icon="mdi:email"/>
+                <FishButton big simple newTab to="https://buy.stripe.com/aEUaGJ7iH1aO9e84gg" title="Buy me a coffee" icon="mdi:coffee"/>
             </div>
         </div>
     </header>
