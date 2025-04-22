f19d207cc639fc6c973407248bd0431baef2e0f7
Chris Oleson <chris@oleson.family>
2025-04-21 19:05:39 -0600

    cleaning up code

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
-<section class="full height section">
-    <div class="picture">
-        <h1>Hi, I'm Chris.</h1>
-        <nuxt-img src="/images/profile-pic.jpg" height="200" width="200" alt="Chris Oleson"/>
-    </div>
-    <h2>
-        I'm a web developer, UI designer, and entrepreneur. Technology fascinates me; it's the perfect blend of creativity and problem-solving.
-        If you're building something on the internet, let's talk!
-    </h2>
-</section>
+    <section class="full height section">
+        <div class="picture">
+            <h1>Hi, I'm Chris.</h1>
+            <NuxtImg src="/images/profile-pic.jpg" height="200" width="200" alt="Chris Oleson"/>
+        </div>
+        <h2>
+            I'm a web developer, UI designer, and entrepreneur. Technology fascinates me; it's the perfect blend of creativity and problem-solving.
+            If you're building something on the internet, let's talk!
+        </h2>
+    </section>
 </template>
 
 <script setup>
